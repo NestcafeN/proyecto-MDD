727fffdc2e989ec34029aede8a552a9197dba2ae
ShompiFlen <shompire02@gmail.com>
2023-06-28 17:22:08 -0400

    cors

--- api/index.js
+++ api/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const server = express()
+const cors = require('cors')
 const PORT = 1234
+
+const jsonParser = bodyParser.json()
+server.use(cors({
+  origin: "http://localhost:3000"
+}))
 
 server.get("/", (request, response) => {
   response.status(200).json({
@@ -8,7 +15,7 @@ server.get("/", (request, response) => {
   })
 })
 
-server.post("cursos/create", (req, res) => {
+server.post("/cursos/create", jsonParser, (req, res) => {
 
   console.log(req.body);
 
@@ -18,13 +25,13 @@ server.post("cursos/create", (req, res) => {
   })
 })
 
-server.delete("cursos/delete", (req, res) => {
+server.delete("/cursos/delete", (req, res) => {
   res.status(200).json({
     message: 'Petición recibida con exito'
   })
 })
 
-server.patch("cursos/edit", (req, res) => {
+server.patch("/cursos/edit", (req, res) => {
   res.status(200).json({
     message: 'Petición recibida con exito'
   })
