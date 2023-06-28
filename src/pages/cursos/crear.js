472ebd8b08a7ab3f148f1589845fd6a3154fbf03
ShompiFlen <shompire02@gmail.com>
2023-06-28 17:22:16 -0400

    Enviar info del curso desde el front

--- src/pages/cursos/crear.js
+++ src/pages/cursos/crear.js
@@ -2,12 +2,29 @@ import styles from "./crear.module.css"
 
 export default function Crear() {
 
-  function handleCreation(e) {
-    e.preventDefault()
+  async function handleCreation(e) {
+    e.preventDefault();
 
-    console.log(e)
+    console.log(e);
+    // Obtener la info del formulario
+    const values = {
+      nombre: e.target["0"].value,
+      descripcion: e.target["1"].value,
+      fecha_inicio: e.target["2"].value,
+      fecha_termino: e.target["3"].value,
+      cupos: e.target["4"].value
+
+    }
+    console.log(values);
+
+    await fetch("http://localhost:1234/cursos/create", {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
   }
-
   return (
     <div>
       <h1>Creación de un curso</h1>
